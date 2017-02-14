e47aeaf0dd19584c926e176592266d32991564f2
lionely <lionelscotty95@gmail.com>
2017-02-14 16:55:43 -0600

    Add Initial Project
    Added initial project

---
+++ COMP-255 practice/Assets/Scripts/Generate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour {
+	public GameObject veggie1;
+	public GameObject veggie2;
+
+	public GameObject meat1;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void CreateObstacle(){
+		Instantiate (veggie1);
+		Instantiate (veggie2);
+		Instantiate (meat1);
+	}
+}
---
+++ COMP-255 practice/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+	private Rigidbody2D rigi;
+	public Vector2 velocity = new Vector2 (0, -4);
+	public float range = 4; // Range of random variation
+
+	private void Awake(){
+		rigi = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigi.velocity = velocity; // sets velocity to the one above.
+		//transform.position = new Vector3 (transform.position.x - range * Random.value, 
+			//transform.position.y , transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
