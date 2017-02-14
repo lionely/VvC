53d3fccb3eb861fb4b074e5763a2329e6d5345a2
jeremymoss <jmoss@macalester.edu>
2017-02-14 17:17:04 -0600

    Add SwipeManager Script

---
+++ COMP-255 practice/Assets/Scripts/SwipeManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeManager : MonoBehaviour {
+	private Touch initialTouch = new Touch();
+	private float distance = 0;
+	private bool hasSwiped = false;
+	private int pos = 0;
+
+	void FixedUpdate()
+	{
+		foreach(Touch t in Input.touches)
+		{
+			if (t.phase == TouchPhase.Began)
+			{
+				initialTouch = t;
+			}
+			else if (t.phase == TouchPhase.Moved && !hasSwiped)
+			{
+				float deltaX = initialTouch.position.x - t.position.x;
+				float deltaY = initialTouch.position.y - t.position.y;
+				distance = Mathf.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
+				bool swipedSideways = Mathf.Abs(deltaX) > Mathf.Abs(deltaY);
+
+				if (distance > 100f)
+				{
+					if (swipedSideways && deltaX > 0) //&& pos != -6) //swiped left
+					{
+						this.transform.position = new Vector3(this.transform.position.x - 0.5f, 
+							this.transform.position.y, -1);
+						pos = pos - 1;
+					}
+					else if (swipedSideways && deltaX <= 0) //&& pos != 6) //swiped right
+					{
+						this.transform.position = new Vector3(this.transform.position.x + 0.5f, 
+							this.transform.position.y, -1);
+						pos = pos + 1;
+					}
+					else if (!swipedSideways && deltaY > 0) //swiped down
+					{
+						this.transform.position = new Vector3(this.transform.position.x, 
+							this.transform.position.y - 0.5f, -1);					
+					}
+					else if (!swipedSideways && deltaY <= 0)  //swiped up
+					{
+						this.transform.position = new Vector3(this.transform.position.x, 
+							this.transform.position.y + 0.5f, -1);
+					}
+					hasSwiped = true;
+				}
+
+			}
+			else if (t.phase == TouchPhase.Ended)
+			{
+				initialTouch = new Touch();
+				hasSwiped = false;
+			}
+		}
+	}
+}
