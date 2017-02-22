8e1cf7389370b2b3c53a64a9e9d2fcdfa84b0feb
Tianyi Liu <tliu@macalester.edu>
2017-02-21 21:36:19 -0600

    added pause button
    We still need to fix player movement. The button is located at top left
    and the player moves to the left before the game pauses if the player
    is not in the left most lane.

---
+++ COMP-255 practice/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public bool paused;
+
+	// Use this for initialization
+	void Start () {
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Pause () {
+
+		Touch t = Input.GetTouch (0);
+
+		if (t.phase == TouchPhase.Ended) {	//At end of touch
+			paused = !paused;
+		}
+
+		if (paused) {
+			Time.timeScale = 0;
+		}
+
+		else if (!paused) {
+			Time.timeScale = 1;
+		}
+	}
+}
