fb7c8921870b72feeaec9e539c295d5a8cae2553
cascadianblue <connor.bo@gmail.com>
2017-03-01 17:09:44 -0600

    Set Background to Move at Global Speed
    The background now moves at (roughly) the same (accelerating) speed as
    the game objects. Due to unknown reasons, we have to multiply the
    canonical "object" speed by a constant factor. Let's figure out why
    that's necessary ASAP.

--- COMP-255 practice/Assets/Scripts/UVscroll.cs
+++ COMP-255 practice/Assets/Scripts/UVscroll.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UVscroll : MonoBehaviour {
-
+	/*
+	 * Scrolls the background
+	 */
 
 	public Vector2 uvSpeed = new Vector2 (0.0f, 1.0f);
 	Renderer renderer;
 	Vector2 uvOffset = Vector2.zero;
+
+	// The ratio of the "speed" variable in the GameController to the y-movement
+	// in this script (to make it look equal to the user)
+	private float SPEED_FACTOR = 0.04f;
+
+	GameController gc;
 	// Use this for initialization
 	void Start () {
 		renderer = GetComponent<Renderer>();
+		gc = GameController.FindObjectOfType<GameController> ();
 	}
-	
-	// Update is called once per frame *Check
-	void LateUpdate () {
-		uvOffset += (uvSpeed * Time.deltaTime);
+
+	// Called once per frame, after all other update methods have been called
+	void Update () {
+		uvOffset += (new Vector2(0.0f, -gc.speed*SPEED_FACTOR) * Time.deltaTime);
 		renderer.materials[0].SetTextureOffset ("_MainTex", uvOffset);
 	}
 }
