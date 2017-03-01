61e564ba71d57d4e7330a14756d38cedaac7c8de
cascadianblue <connor.bo@gmail.com>
2017-03-01 17:09:17 -0600

    Extract Constants and Add Comment

--- COMP-255 practice/Assets/Scripts/Obstacle.cs
+++ COMP-255 practice/Assets/Scripts/Obstacle.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
+	/**
+	 * Causes the Obstacles (i.e. the veggies and the meat) to scroll down
+	 */
+
+	private int START_Y = 9; // Should be the top of the screen
+	private float LEFTLANE_X = -2.25f;
+	private float MIDLANE_X = 0.0f;
+	private float RIGHTLANE_X = 2.25f;
 	private Rigidbody2D rigi;
 	private GameController gc;
 	private float downward_speed;
@@ -14,26 +22,26 @@ public class Obstacle : MonoBehaviour {
 	// Called when the obstacle is generated
 	private void Awake(){
 		rigi = GetComponent<Rigidbody2D>();
+		// Retrieve the active instance of GameController, which has information on the game state (e.g. speed)
 		gc = GameController.FindObjectOfType<GameController> ();
 		rigi.velocity = new Vector2(0.0f, gc.speed);
 		foodPosX     = new float[3];
-		foodPosX [0] = -2.25f;
-		foodPosX [1] =  0f;
-		foodPosX [2] = 2.25f;
-
-		
+		foodPosX [0] = LEFTLANE_X;
+		foodPosX [1] =  MIDLANE_X;
+		foodPosX [2] = RIGHTLANE_X;
 	}
 
 	// Use this for initialization of an Obstacle
 	void Start () {
 		index = Random.Range (0, 3);
-		transform.position = new Vector2 (foodPosX [index], 9); // Decided to put foods at y = 9.
+		transform.position = new Vector2 (foodPosX [index], START_Y); // Decided to put foods at y = 9.
 
 
 	}
 
 	// Called once per frame
 	void Update () {
+		// Set the speed of this object to the speed of the scene
 		rigi.velocity = new Vector2 (0.0f, gc.speed);
 	}
 
