d3fba4a60e17626b9a3ff84debcd89d8c6e03e9e
jeremymoss <jmoss@macalester.edu>
2017-03-22 13:29:01 -0500

    Added dynamic decoration spawning.

---
+++ COMP-255 practice/Assets/Scripts/Decoration.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decoration : MonoBehaviour {
+
+	private int START_Y = 9; // Should be the top of the screen
+	private float LEFTLANE_X = -4.5f;
+	private float RIGHTLANE_X = 4.5f;
+	private Rigidbody2D rigi;
+	private GameController gc;
+	private float downward_speed;
+	public float[] decorationPosX;
+	private int index;
+
+
+
+	// Called when the obstacle is generated
+	private void Awake(){
+		rigi = GetComponent<Rigidbody2D>();
+		// Retrieve the active instance of GameController, which has information on the game state (e.g. speed)
+		gc = GameController.FindObjectOfType<GameController> ();
+		rigi.velocity = new Vector2(0.0f, gc.speed);
+		decorationPosX     = new float[2];
+		decorationPosX [0] = LEFTLANE_X;
+		decorationPosX [1] = RIGHTLANE_X;
+	}
+
+	// Use this for initialization of an Obstacle
+	void Start () {
+		index = Random.Range (0, 2);
+		transform.position = new Vector2 (decorationPosX [index], START_Y);
+
+
+	}
+
+	// Called once per frame
+	void Update () {
+		// Set the speed of this decoration to the speed of the scene
+		rigi.velocity = new Vector2 (0.0f, gc.speed);
+	}
+}
