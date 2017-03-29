12b53a177545ecebb1cd94c4297f07a9809f37f5
cascadianblue <connor.bo@gmail.com>
2017-03-29 13:59:46 -0500

    Alter Object Generation to Match Speed
    Change Generate.cs to make object generation scale with speed, so that
    it looks like objects are at uniform density and the player is speeding
    up.

--- COMP-255 practice/Assets/Scripts/Generate.cs
+++ COMP-255 practice/Assets/Scripts/Generate.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 
 public class Generate : MonoBehaviour {
 
+	private GameController gc;
 	public GameObject[] veggie = new GameObject[2];
 	public GameObject[] meat = new GameObject[2];
 	public GameObject[] mushroom = new GameObject[1];
-
 	public GameObject decoration;
+	public int frameCount = 0;
+
+
+	// Determines the relationship of GameController speed to Obstacle generation rate; higher = faster
+	private float OBSTACLE_GENERATION_CONSTANT = 0.01f;
+
+	// Same as above, for Decorations
+	private float DECORATION_GENERATION_CONSTANT = 0.005f;
+
+	// Stores the amount of time to go (unitless, sorry) until another Obstacle or Decoration is generated
+	private float timeTillObstacle;
+	private float timeTillDecoration;
 
 	private int index;
 	private int chanceVM; // if 0 make meat, else veggie
 
 	// Use this for initialization
 	void Start () {
-		//print (chanceVM);
-		InvokeRepeating ("CreateObstacle", 1f, .5f);
-		InvokeRepeating ("CreateDecoration", 1f, 3f);
+		gc = GameController.FindObjectOfType<GameController> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		chanceVM = Random.Range (0, 11); // 0-10 
 		index = Random.Range (0, 2); // 0 or 1
+		timeTillObstacle -= OBSTACLE_GENERATION_CONSTANT;
+		timeTillDecoration -= DECORATION_GENERATION_CONSTANT;
+		if (timeTillObstacle <= 0.0f) {
+			CreateObstacle ();
+			timeTillObstacle = 1.0f / (-gc.speed);
+		}
+		if (timeTillDecoration <= 0.0f) {
+			CreateDecoration ();
+			timeTillDecoration = 1.0f / (-gc.speed);
+		}
 	}
+
+
 	void CreateObstacle(){
 		//print (chanceVM);
 		// 45.5% Veggie
