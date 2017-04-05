31a449983633441d94f90351753fa9a6e2161ab1
Tianyi Liu <tliu@macalester.edu>
2017-04-04 20:40:00 -0500

    Cleaned up more code in Player script. Renamed the project file from "comp225" to "VvC"

---
+++ VvC/Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public GameController gameController;
+	public Animator animator;
+
+	public GameObject veganBackground;
+	public GameObject meatBackground;
+
+	public static bool Vegan;
+
+	private void Awake(){
+		
+	}
+		
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		setVegan ();// to start as Vegan or not.
+		if (!Vegan) {
+			switchBackground ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//Sets Initial Mode
+	void setVegan(){
+		if (Vegan) {
+			animator.SetTrigger ("toVegan");
+		} 
+		else 
+		{
+			animator.SetTrigger ("toCarnivore");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+
+		// Vegan Mode
+		if (Vegan) {
+			if (col.gameObject.tag == "Veggie") {
+				Destroy(col.gameObject,.1f);
+				gameController.AddScore();
+			}
+			else if (col.gameObject.tag == "Meat") {
+				Destroy(col.gameObject,.1f);
+				gameController.LoseLife();
+			}
+			else { // Mushroom 
+				Destroy(col.gameObject,.1f);
+				switchBackground() ;
+				Vegan = !Vegan;
+				animator.SetTrigger ("toCarnivore");
+			}
+		}
+
+		// Canivore Mode
+		else {
+			if (col.gameObject.tag == "Veggie") {
+				Destroy(col.gameObject,.1f);
+				gameController.LoseLife();
+			}
+			else if (col.gameObject.tag == "Meat") {
+				Destroy(col.gameObject,.1f);
+				gameController.AddScore();
+			}
+			else { // Mushroom
+				Destroy(col.gameObject,.1f);
+				switchBackground ();
+				Vegan = !Vegan;
+				animator.SetTrigger ("toVegan");
+			}
+		}
+	}
+
+	public void DestroyPlayer() {
+		Destroy(this.gameObject);
+	}
+
+	public void switchBackground(){
+		if (veganBackground.transform.position.x == 0.0f) {
+			meatBackground.transform.position = new Vector3 (0.0f, -1.17f, 0.0f);
+			veganBackground.transform.position = new Vector3 (-15.0f, -1.17f, 0.0f);
+		} else {
+			veganBackground.transform.position = new Vector3 (0.0f, -1.17f, 0.0f);
+			meatBackground.transform.position = new Vector3 (-15.0f, -1.17f, 0.0f);;
+		}
+	}
+}
---
+++ VvC/Assets/Scripts/mainmenu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class mainmenu : MonoBehaviour {
+
+	GameObject music;
+	public GameObject levelLoader;
+
+
+	void Start()
+	{
+		PlayerPrefs.GetInt ("HighScore");// loads the highscore from memory
+	}
+
+	/*
+	 * This method is called by the Quit Button. It quits the game.
+	 */
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	/*
+	 * Displays HighScores
+	 */
+	public void HighScore()
+	{
+		SceneManager.LoadScene ("HighScore",LoadSceneMode.Single);//loads scenes
+	}
+
+	public void ChooseCarnivore()
+	{
+		Player.Vegan = false;
+		SceneManager.LoadScene ("GamePlay",LoadSceneMode.Single);//loads scenes
+	}
+
+	public void ChooseVegan()
+	{
+		Player.Vegan = true;
+		SceneManager.LoadScene ("GamePlay",LoadSceneMode.Single);//loads scenes
+	}
+
+}
+
+
+
+
+
+
+
