76a5a83a13e4ce66a70f26acea5ec108c0bf004a
lionely <lionelscotty95@gmail.com>
2017-04-23 17:25:48 -0500

    Title screen update
    new art

---
+++ VvC/Assets/Scripts/FoodFall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodFall : MonoBehaviour {
+
+	public GameObject[] food = new GameObject[6];
+	private int foodIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foodIndex = Random.Range (0, 5);
+		Instantiate (food [foodIndex]);
+	}
+}
